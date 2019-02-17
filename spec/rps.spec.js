f0db5e6a93eb714770c6be2ae49b4512d7d82e7c
Laura Reale <laaru1988@gmail.com>
2019-02-17 17:58:10 +0100

    add tests and implementation code to check all possible rps game results

--- spec/rps.spec.js
+++ spec/rps.spec.js
@@ -7,21 +7,21 @@ describe('Rps', () => {
         expect(rps.check('Banana', 'Apple')).to.eql('You must choose Rock, Paper or Scissors!')
     })
 
-    it('returns a random computer choice', () => {
-        computerOptions = ['Rock', 'Paper', 'Scissors']
-        expect(computerOptions.randomChoice()).to.eql('Rock' || 'Paper' || 'Scissors')
-    })
-
     it('returns Paper wins if Rock vs Paper', () => {
-        expect(rps.check(Rock, Paper)).to.eql('Paper wins')
+        expect(rps.check('Rock', 'Paper')).to.eql('Paper wins')
     })
 
     it('returns Rock wins if Rock vs Scissors', () => {
-        expect(rps.check(Rock, Scissors)).to.eql('Rock wins')
+        expect(rps.check('Rock', 'Scissors')).to.eql('Rock wins')
     })
 
     it('returns Scissors wins if Paper vs Scissors', () => {
-        expect(rps.check(Paper, Scissors)).to.eql('Scissors wins')
+        expect(rps.check('Paper', 'Scissors')).to.eql('Scissors wins')
+    })
+
+    it('returns a random computer choice', () => {
+        computerOptions = ['Rock', 'Paper', 'Scissors']
+        expect(computerOptions.randomChoice()).to.eql('Rock' || 'Paper' || 'Scissors')
     })
 
 })
--- src/js/rps.js
+++ src/js/rps.js
@@ -1,5 +1,13 @@
 function Rps()  {
     this.check = (string1, string2) => {
-        return 'You must choose Rock, Paper or Scissors!'
+        if (string1 == 'Rock' && string2 == 'Paper') {
+            return 'Paper wins';
+        } else if (string1 == 'Rock' && string2 == 'Scissors') {
+            return 'Rock wins';
+        } else if (string1 == 'Paper' && string2 == 'Scissors') {
+            return 'Scissors wins'
+        } else {
+            return 'You must choose Rock, Paper or Scissors!'
+        }
     }
 }
