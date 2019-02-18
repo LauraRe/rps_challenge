ca6b056b72d07838a3307c90512f21d808a6f9a1
Laura Reale <laaru1988@gmail.com>
2019-02-18 22:15:51 +0100

    create twoPlayers.html file for displaying two-players game, add link to game on index.html, update feature test

--- features/application.feature.js
+++ features/application.feature.js
@@ -14,7 +14,8 @@ describe('User can input a string value and get Rps results ', () => {
     browser.close();
   });
 
-  it('clicking on the "Check" button', async () => {
+  it('clicking on the "Check" button in two-players game', async () => {
+    await browser.clickOnButton('a[href="twoPlayers.html"]')
     await browser.fillIn("input[id='value1']", { with: "Rock" })
     await browser.fillIn("input[id='value2']", { with: "Paper" })
     await browser.clickOnButton("input[value='Check']")
