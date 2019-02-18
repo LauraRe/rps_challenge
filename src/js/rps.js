14f0d6a20a5667a34768c6416dee96485efa8077
Laura Reale <laaru1988@gmail.com>
2019-02-18 09:34:44 +0100

    fix input capitalization issues

--- src/js/rps.js
+++ src/js/rps.js
@@ -1,12 +1,14 @@
 function Rps()  {
     this.check = (string1, string2) => {
-        if (string1 == 'Rock' && string2 == 'Paper') {
+        str1 = string1.toLowerCase()
+        str2 = string2.toLowerCase()
+        if (str1 == 'rock' && str2 == 'paper') {
             return 'Paper wins';
-        } else if (string1 == 'Rock' && string2 == 'Scissors') {
+        } else if (str1 == 'rock' && str2 == 'scissors') {
             return 'Rock wins';
-        } else if (string1 == 'Paper' && string2 == 'Scissors') {
+        } else if (str1 == 'paper' && str2 == 'scissors') {
             return 'Scissors wins';
-        } else if (string1 == string2) {
+        } else if (str1 == str2) {
             return 'Even!';
         } else {
             return 'You must choose Rock, Paper or Scissors!';
