f480331ec42a8ddf0ddb1f99b6d542ac88b72158
Laura Reale <laaru1988@gmail.com>
2019-02-21 01:01:21 +0100

    add footer in index.html, fix one-player feature test

--- features/application.feature.js
+++ features/application.feature.js
@@ -14,17 +14,19 @@ describe('User can input a string value and get Rps results ', () => {
   });
 
   it('clicking on the "Check" button in two-players game', async () => {
-    await browser.clickOnButton('a[href="twoPlayers.html"]')
+    await browser.clickOnButton("#two")
     await browser.fillIn("input[id='value1']", { with: "Rock" })
+    await browser.clickOnButton("#submit1")
     await browser.fillIn("input[id='value2']", { with: "Paper" })
-    await browser.clickOnButton("input[value='Check']")
+    await browser.clickOnButton("#submit2")
+    await browser.clickOnButton("#resultButton")
     let content = await browser.getContent("[id='display_answer']")
     expect(content).to.eql('Paper wins');
   })
 
   it('clicking on the "Check" button in one-player game', async () => {
-    await browser.clickOnButton('a[href="onePlayer.html"]')
-    // await browser.select("input[id='value']", { with: "rock" })
+    await browser.clickOnButton("#one")
+    await browser.selectOption("#value", { option: "rock" })
     await browser.clickOnButton("input[value='Check']")
     let content = await browser.getContent("[id='display_answer']")
     expect(content).to.be.a('string');
